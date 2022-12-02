523a1257477fa097da1dd4aa386713c672e3e2d4
abindran <abindranr@gmail.com>
2022-12-02 21:20:32 +0530

    The Completed e-commerce application

---
+++ projbackend/controllers/paymentb.js
@@ -0,0 +1,41 @@
+var braintree = require("braintree");
+
+var gateway = braintree.connect({
+	environment: braintree.Environment.Sandbox,
+	merchantId: "vst28r3m6m9t6ffd",
+	publicKey: "4jv6d8vtmkcxbyfp",
+	privateKey: "51acc602414947bda4c53851c683b770",
+});
+
+exports.getToken = (req, res) => {
+  gateway.clientToken.generate({}, function(err, response) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(response);
+    }
+  });
+};
+
+exports.processPayment = (req, res) => {
+  let nonceFromTheClient = req.body.paymentMethodNonce;
+
+  let amountFromTheClient = req.body.amount;
+  gateway.transaction.sale(
+    {
+      amount: amountFromTheClient,
+      paymentMethodNonce: nonceFromTheClient,
+
+      options: {
+        submitForSettlement: true
+      }
+    },
+    function(err, result) {
+      if (err) {
+        res.status(500).json(error);
+      } else {
+        res.json(result);
+      }
+    }
+  );
+};
